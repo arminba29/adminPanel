0891b64b2d865a8fc50590731defb6a3fdc23774
Armin Barari <armin.barariii@gmail.com>
2023-10-02 23:24:56 +0330

    install ant design

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
-
+const handleMessage = () => {
+  message.info("This is a normal message");
+}
 definePageMeta({
   layout: 'admin-layout'
 })
+
 </script>
 
 <template>
   <span>index</span>
+  <a-button @click="handleMessage">
+    button
+  </a-button>
 </template>
 
 <style scoped>
