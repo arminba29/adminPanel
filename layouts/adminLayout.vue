701623452cafe53681de070bdbb9f1c68bbadf1a
Armin Barari <armin.barariii@gmail.com>
2023-10-02 23:13:02 +0330

    create project

---
+++ layouts/adminLayout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  header
+<slot></slot>
+  footer
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+definePageMeta({
+  layout: 'admin-layout'
+})
+</script>
+
+<template>
+  <span>index</span>
+</template>
+
+<style scoped>
+
+</style>
